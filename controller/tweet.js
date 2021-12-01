8c7bd38117404bebdfd0ca1b338d5a576f118cc5
redpandaathome <leeyumi415@gmail.com>
2021-12-01 12:01:09 +0900

    controller - tweet

--- controller/tweet.js
+++ controller/tweet.js
@@ -6,3 +6,44 @@ export async function getTweets(req,res,next){
    let data = await (username ? tweetRepository.getAllByUsername(username) : tweetRepository.getAll());
    res.status(200).json(data);
 }
+
+export function getById(req,res,next){
+   const id = req.params.id;
+   const tweet = tweetRepository.getById(id)
+   if(tweet){
+      res.status(200).json(tweet);
+   } 
+   // else needed due to this problem - "Cannot set headers after they are sent to the client"
+   else {
+      res.status(404).json({message:`Tweet id ${id} not found`})
+   }
+}
+
+export function createTweet(req,res,next){
+   let {text, name, username} = req.body;
+   const tweet = {
+      text,
+      name,
+      username
+   }
+   let newTweet = tweetRepository.create(tweet);
+   res.status(201).json(newTweet);
+}
+
+export function updateTweet(req, res, next){
+   const id = req.params.id;
+   const text = req.body.text;
+   
+   let tweet = tweetRepository.update(id, text);
+   if(tweet){
+      res.status(200).json(tweet);
+   } else {
+      res.status(404).json({message: `Tweet id ${id} not found`})
+   }
+}
+
+export function deleteTweet(req, res, next){
+   const id = req.params.id;
+   tweetRepository.remove(id);
+   res.sendStatus(204);
+}
--- router/dwitter.js
+++ router/dwitter.js
@@ -5,43 +5,12 @@ const router = express.Router();
 //💜
 router.get("/", tweetController.getTweets);
 
-router.get("/:id", (req, res, next) => {
-   const id = req.params.id;
-   //✨ find, filter...
-   const tweet = tweetRepository.getById(id)
-   if(tweet){
-      res.status(200).json()
-   }
-   res.status(404).json({message:`Tweet id ${id} not found`})
-})
+router.get("/:id", tweetController.getById);
 
-router.post("/", (req, res, next) => {
-   let {text, name, username} = req.body;
-   const tweet = {
-      text,
-      name,
-      username
-   }
-   let newTweet = tweetRepository.create(tweet);
-   res.status(201).json(newTweet);
-})
+router.post("/", tweetController.createTweet);
 
-router.put("/:id", (req, res, next) => {
-   const id = req.params.id;
-   const text = req.body.text;
-   
-   tweet = tweetRepository.update(id, text);
-   if(tweet){
-      res.status(200).json(tweet);
-   } else {
-      res.status(404).json({message: `Tweet id ${id} not found`})
-   }
-})
+router.put("/:id", tweetController.updateTweet);
 
-router.delete("/:id", (req, res, next) => {
-   const id = req.params.id;
-   tweetRepository.remove(id);
-   res.sendStatus(204);
-})
+router.delete("/:id", tweetController.deleteTweet);
 
 export default router;
