f4e10c745c6996b8be1c091b00393bc7e923f205
redpandaathome <leeyumi415@gmail.com>
2021-12-02 23:05:32 +0900

    apply - validator

--- middleware/validator.js
+++ middleware/validator.js
@@ -1,11 +1,9 @@
 import { validationResult } from "express-validator";
 
-export const validate = (req, res, next)=>{
-   const result = validationResult(req);
-   const hasErrors = !result.isEmpty();
-   if(hasErrors){
-      console.log(result.errors[0].msg)
-      return res.status(404).json({msg: result.errors[0].msg})
-   }
-   return next()
-}
+export const validate = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ message: result.errors[0].msg });
+  }
+  return next();
+};
--- router/twitter.js
+++ router/twitter.js
@@ -1,28 +1,36 @@
 import express from "express";
-import { body } from "express-validator";
-import {validate} from "../middleware/validator.js";
+import { body, query } from "express-validator";
+import { validate } from "../middleware/validator.js";
 import * as tweetController from "../controller/tweet.js";
 const router = express.Router();
 
+const validateTweet = [
+  body("text")
+    .trim()
+    .isLength({ min: 3 })
+    .withMessage("Text can not be shorter than 3 characters"),
+  validate,
+];
 
 //💜
-router.get("/", tweetController.getTweets);
+router.get(
+  "/",
+  [
+    query("username")
+      .trim()
+      .isLength({ min: 2 })
+      .withMessage("username must be more than 2 letters")
+      .optional({ nullable: true, checkFalsy: true }),
+      validate,
+  ],
+  tweetController.getTweets
+);
 
 router.get("/:id", tweetController.getById);
 
-router.post(
-  "/",
-  [
-    body("text")
-      .trim()
-      .isLength({ min: 3 })
-      .withMessage("Text can not be shorter than 3 characters"),
-      validate
-  ],
-  tweetController.createTweet
-);
+router.post("/", validateTweet, tweetController.createTweet);
 
-router.put("/:id", tweetController.updateTweet);
+router.put("/:id", validateTweet, tweetController.updateTweet);
 
 router.delete("/:id", tweetController.deleteTweet);
 
